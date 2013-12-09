14fdb967ab64d21a3bd2fcb42b6a1d51f2aa6df1
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-12-09 10:16:55 +0100

    fix abstract controller

--- src/Brick/Controller/AbstractController.php
+++ src/Brick/Controller/AbstractController.php
@@ -2,11 +2,14 @@
 
 namespace Brick\Controller;
 
+use Brick\PimpleAware;
+use Pimple;
+
 abstract class AbstractController implements PimpleAware
 {
     protected $pimple;
 
-    public function setPimple(Pimple $pimple)
+    public function setPimple(Pimple $pimple = null)
     {
         $this->pimple = $pimple;
     }
