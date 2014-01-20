b09203e077abc51d386643ad9a1961c3676648f4
Henrik Bjørnskov <henrik@bjrnskov.dk>
2014-01-20 16:06:55 +0100

    Add Route annotation that lets the annotation specify a id instead of always using the class notation

--- src/Brick/Routing/AnnotationClassLoader.php
+++ src/Brick/Routing/AnnotationClassLoader.php
@@ -3,11 +3,16 @@
 namespace Brick\Routing;
 
 use Symfony\Component\Routing\Route;
+use Brick\Annotation\Route as BrickRoute;
 
 class AnnotationClassLoader extends \Symfony\Component\Routing\Loader\AnnotationClassLoader
 {
     protected function configureRoute(Route $route, \ReflectionClass $class, \ReflectionMethod $method, $annot)
     {
-        $route->setDefault('_controller', $class->getName() . '::' . $method->getName());
+        if ($annot instanceof BrickRoute) {
+            $route->setDefault('_controller', $annot->getService() . ':' . $method->getName());
+        } else {
+            $route->setDefault('_controller', $class->getName() . '::' . $method->getName());
+        }
     }
 }
