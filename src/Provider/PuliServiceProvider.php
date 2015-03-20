bedf35ec5e1a417477460a46e71931dc7885ab57
Henrik Bjornskov <hb@peytz.dk>
2015-03-20 08:10:07 +0100

    Add the puli template loader if twig is present

--- src/Provider/PuliServiceProvider.php
+++ src/Provider/PuliServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Brick\Provider;
 
 use Pimple\Container;
+use Puli\Extension\Twig\PuliTemplateLoader;
 
 class PuliServiceProvider implements \Pimple\ServiceProviderInterface
 {
@@ -24,5 +25,14 @@ class PuliServiceProvider implements \Pimple\ServiceProviderInterface
         $app['puli.discovery'] = function ($app) {
             return $app['puli.factory']->createDiscovery($app['puli.repository']);
         };
+
+        if (isset($app['twig.loader'])) {
+            $app->extend('twig.loader', function ($loader, $app) {
+                $loader->addLoader(new PuliTemplateLoader($app['puli.repository']));
+
+                return $loader;
+
+            });
+        }
     }
 }
